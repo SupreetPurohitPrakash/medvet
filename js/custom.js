51b3309287a665af62ffa37425d38c352d4a7859
Supreet <supreet.official2020@gmail.com>
2021-08-08 19:58:44 +0545

    minor changes

--- js/custom.js
+++ js/custom.js
@@ -22,68 +22,68 @@ jQuery(document).ready(function ($) {
     ]
   });
 
-  $('.partner-col a').click(function (event) {
-    event.preventDefault();
-  })
+  // $('.partner-col a').click(function (event) {
+  //   event.preventDefault();
+  // })
 
-  $('.partner-col').click(function () {
-    const info = $(this).find('.partner-info'),
-      cardText = $(this).find('.partner-card .text'),
-      allInfo = $('.partner-info'),
-      allCardText = $('.partner-card .text');
+  // $('.partner-col').click(function () {
+  //   const info = $(this).find('.partner-info'),
+  //     cardText = $(this).find('.partner-card .text'),
+  //     allInfo = $('.partner-info'),
+  //     allCardText = $('.partner-card .text');
 
-    if ($(this).hasClass('active')) {
-      return;
-    }
+  //   if ($(this).hasClass('active')) {
+  //     return;
+  //   }
 
-    // closing all
-    $('.partner-col').removeClass('active');
-    allCardText.slideDown();
-    allInfo.slideUp();
-    $('.partner-col').css('order', '0');
+  //   // closing all
+  //   $('.partner-col').removeClass('active');
+  //   allCardText.slideDown();
+  //   allInfo.slideUp();
+  //   $('.partner-col').css('order', '0');
 
-    // opening present
-    $(this).css('order', '-1');
-    $(this).addClass('active');
-    cardText.slideUp();
-    info.slideDown();
+  //   // opening present
+  //   $(this).css('order', '-1');
+  //   $(this).addClass('active');
+  //   cardText.slideUp();
+  //   info.slideDown();
 
 
-    $("html, body")
-      .stop()
-      .animate({
-          scrollTop: $('.section-partners').offset().top,
-        },
-        500
-      );
-  })
+  //   $("html, body")
+  //     .stop()
+  //     .animate({
+  //         scrollTop: $('.section-partners').offset().top,
+  //       },
+  //       500
+  //     );
+  // })
+  // 
+  // if ($('body').hasClass('page-partners')) {
+  //   $(".nav-list .has-submenu a").each(function () {
+  //     let link = $(this).attr('href');
+  //     link = link.split('#').pop();
+  //     console.log(link);
+  //     $(this).prop('href', '#' + link);
+  //   });
 
-  if ($('body').hasClass('page-partners')) {
-    $(".nav-list .has-submenu a").each(function () {
-      let link = $(this).attr('href');
-      link = link.split('#').pop();
-      console.log(link);
-      $(this).prop('href', '#' + link);
-    });
+  //   $(".nav-list .has-submenu a").bind("click", function (event) {
+  //     var $anchor = $(this);
 
-    $(".nav-list .has-submenu a").bind("click", function (event) {
-      var $anchor = $(this);
+  //     if ($(".header")[0]) {
+  //       topSpacer = $(".header").outerHeight() + 50;
+  //     }
 
-      if ($(".header")[0]) {
-        topSpacer = $(".header").outerHeight() + 50;
-      }
+  //     $("html, body")
+  //       .stop()
+  //       .animate({
+  //           scrollTop: $($anchor.attr("href")).offset().top - 20,
+  //         },
+  //         1000
+  //       );
 
-      $("html, body")
-        .stop()
-        .animate({
-            scrollTop: $($anchor.attr("href")).offset().top - 20,
-          },
-          1000
-        );
-
-      event.preventDefault();
-    });
-  }
+  //     event.preventDefault();
+  //   });
+  // }
 
   function navToggle() {
     $('.nav-toggle,.nav-bg').click(function () {
