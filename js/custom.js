d44917600cd751f1dc1a5c25455818e7077ce84a
Supreet <supreet.official2020@gmail.com>
2021-08-12 00:54:47 +0545

    news pages

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function ($) {
 
+  //smooth scroll
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+
+    $("html, body")
+      .stop()
+      .animate({
+          scrollTop: $($anchor.attr("href")).offset().top - 50,
+        },
+        1000
+      );
+
+    event.preventDefault();
+  });
+
   $('.bloghp-slider').slick({
     infinite: true,
     arrows: true,
@@ -21,69 +36,6 @@ jQuery(document).ready(function ($) {
       }
     ]
   });
-
-  // $('.partner-col a').click(function (event) {
-  //   event.preventDefault();
-  // })
-
-  // $('.partner-col').click(function () {
-  //   const info = $(this).find('.partner-info'),
-  //     cardText = $(this).find('.partner-card .text'),
-  //     allInfo = $('.partner-info'),
-  //     allCardText = $('.partner-card .text');
-
-  //   if ($(this).hasClass('active')) {
-  //     return;
-  //   }
-
-  //   // closing all
-  //   $('.partner-col').removeClass('active');
-  //   allCardText.slideDown();
-  //   allInfo.slideUp();
-  //   $('.partner-col').css('order', '0');
-
-  //   // opening present
-  //   $(this).css('order', '-1');
-  //   $(this).addClass('active');
-  //   cardText.slideUp();
-  //   info.slideDown();
-
-
-  //   $("html, body")
-  //     .stop()
-  //     .animate({
-  //         scrollTop: $('.section-partners').offset().top,
-  //       },
-  //       500
-  //     );
-  // })
-  // 
-  // if ($('body').hasClass('page-partners')) {
-  //   $(".nav-list .has-submenu a").each(function () {
-  //     let link = $(this).attr('href');
-  //     link = link.split('#').pop();
-  //     console.log(link);
-  //     $(this).prop('href', '#' + link);
-  //   });
-
-  //   $(".nav-list .has-submenu a").bind("click", function (event) {
-  //     var $anchor = $(this);
-
-  //     if ($(".header")[0]) {
-  //       topSpacer = $(".header").outerHeight() + 50;
-  //     }
-
-  //     $("html, body")
-  //       .stop()
-  //       .animate({
-  //           scrollTop: $($anchor.attr("href")).offset().top - 20,
-  //         },
-  //         1000
-  //       );
-
-  //     event.preventDefault();
-  //   });
-  // }
 
   function navToggle() {
     $('.nav-toggle,.nav-bg').click(function () {
